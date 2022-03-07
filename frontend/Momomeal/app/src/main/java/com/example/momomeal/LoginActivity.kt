ca666756ee0b856221ddfcd0883546cea9ac62a4
Kim Yo Han <rladygks329@gmail.com>
2022-03-07 10:11:25 +0900

    fix: login fagment -> login activity
    fix: login fagment -> login activity

---
+++ frontend/Momomeal/app/src/main/java/com/example/momomeal/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.momomeal
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    fun onLoginBtnClick(v: View?) {
+        val intent = Intent(this, MainActivity::class.java);
+        startActivity(intent)
+        finish()
+    }
+
+}
