f7a3ec6ca27cd7653866d2a06c02a0f539ae054b
Kim Yo Han <rladygks329@gmail.com>
2022-03-14 19:29:54 +0900

    feat: connect login, signup
    - 자동 로그인 추가
    - 전체 동의 기능
    - login, signup 연결
    - textView의 특정 글자 color
    
    수정 사항
    theme -> primary color 변경
    기존 화면을 constraint layout으로 변경
    font추가, drawable rec추가

---
+++ frontend/Momomeal/app/src/main/java/com/example/momomeal/RegisterActivity.kt
@@ -0,0 +1,37 @@
+package com.example.momomeal
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.activity_register_fragment_container, GreetingFragment())
+        transaction.commit()
+    }
+    fun changeFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.activity_register_fragment_container, fragment).commit()
+    }
+    fun initToolbar(toolbar: Toolbar){
+        setSupportActionBar(toolbar);
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar()?.setDisplayShowHomeEnabled(true);
+        getSupportActionBar()?.setDisplayShowTitleEnabled(false)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
