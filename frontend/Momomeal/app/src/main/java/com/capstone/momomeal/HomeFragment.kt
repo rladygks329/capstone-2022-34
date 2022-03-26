0abf6e13c0ac8e6e81ec065ff1fc7e8c28dbf539
Kim Yo Han <rladygks329@gmail.com>
2022-03-27 01:44:43 +0900

    refactor: login, signup, greeting -> Basefragment
    1.
    basefragment와 binding을 사용하게 리팩토링하였습니다 (LoginFragment, GreetingFragment, SignupFragment
    2.
    navi_graph를 원상태로 복구했습니다 (navigate로 createRoom가던거 복구)
    3.
    colors -> color_2을 삭제했습니다
    4.
    create_chat -> google material버튼의 background는 andoird가 아니라 app을 써야해서 수정했습니다.
    5. 중복된 파일을 삭제했습니다 (momomeal/LinearGradientSpan)

--- frontend/Momomeal/app/src/main/java/com/capstone/momomeal/HomeFragment.kt
+++ frontend/Momomeal/app/src/main/java/com/capstone/momomeal/HomeFragment.kt
@@ -1,14 +1,12 @@
 package com.capstone.momomeal
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.capstone.momomeal.databinding.FragmentHomeBinding
 import com.capstone.momomeal.feature.BaseFragment
-import androidx.recyclerview.widget.RecyclerView
 import com.capstone.momomeal.feature.Category
 import com.capstone.momomeal.feature.Chatroom
 import com.capstone.momomeal.feature.ChatroomAdapter
@@ -39,14 +37,16 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
         savedInstanceState: Bundle?
     ): View? {
         val retView = super.onCreateView(inflater, container, savedInstanceState)
-        
-        val recycle = retView?.findViewById<RecyclerView>(R.id.fragment_home_recycler)
+
         val chatroomadapter = ChatroomAdapter(requireContext(), chatroomList)
-        recycle?.adapter = chatroomadapter
+        binding.fragmentHomeRecycler.adapter = chatroomadapter
 
         mainActivity = (activity as MainActivity)
+        val transaction = mainActivity
+            .supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_main_full_container, createChatFragment)
         binding.fabHome.setOnClickListener {
-            mainActivity.findNavController(R.id.fr_main_navi_host).navigate(R.id.createChat)
+            transaction.commit()
         }
         return retView
     }
