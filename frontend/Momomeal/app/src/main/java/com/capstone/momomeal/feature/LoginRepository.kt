609ae17d715f39428f86a66323022c7e1e59b4c5
JinWoo <seanjjang98@gmail.com>
2022-05-15 23:08:12 +0900

    feat: Add login options
    Setting for Chat account test

--- frontend/Momomeal/app/src/main/java/com/capstone/momomeal/feature/LoginRepository.kt
+++ frontend/Momomeal/app/src/main/java/com/capstone/momomeal/feature/LoginRepository.kt
@@ -4,11 +4,15 @@ import com.capstone.momomeal.api.MomomealAPI
 import com.capstone.momomeal.api.MomomealService
 import com.capstone.momomeal.data.User
 import com.capstone.momomeal.data.fakeUser
+import com.capstone.momomeal.data.fakeUsers
 
 class LoginRepository(
     private val momomeal: MomomealAPI = MomomealService.momomealAPI
 ) {
     suspend fun login(email: String, password: String): User? {
-        return if(email == "user1234" && password == "1234") fakeUser else null
+        return if(email == "user1234" && password == "1234") fakeUser
+        else if (email == "ses@naver.com" && password == "qwerty") fakeUsers[0]
+        else if (email == "alal" && password == "12321") fakeUsers[1]
+        else null
     }
 }
